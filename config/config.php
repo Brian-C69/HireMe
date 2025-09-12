2d9efc3d4e20fcdbdf387db3a90538a5fdba8a86
Bernard.C <bchoong1@gmail.com>
2025-09-13 05:00:40 +0800

    removed

--- config/config.php
+++ config/config.php
@@ -18,10 +18,4 @@ return [
         'from_email' => 'choongyunxian@ga2wellness.com',
         'from_name'  => 'HireMe Admin',
     ],
-    'stripe' => [
-        'secret_key'      => 'sk_test_51S6cp6Is8dFmRPkxfdTzbqZelYloc0mX1RnZi8fCcwwf8SSknly87czq7kqJx8tSf1jGg3XpNrP3GZTVq5m5tMUv00BMBb1XYB',
-        'publishable_key' => 'pk_test_51S6cp6Is8dFmRPkxA9GTi4qC15dxdAz2L9qtmkiGZS4BIP3Zp1gVfmiUhKIOfbnDrBfzCdeV009ZlwgprWtjj3ik00JlJCZbzp',
-        'currency'        => 'myr',
-        'webhook_secret'  => 'whsec_xxx',
-    ],
 ];
